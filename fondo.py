e4847eacefee4604244437165d5a79bb3e5e5d08
Ambro17 <ambro17.1@gmail.com>
2017-05-16 12:04:39 -0300

    Agregada persistencia de datos en JSON

--- fondo.py
+++ fondo.py
@@ -4,22 +4,13 @@ import time
 import io
 import os
 from dateutil import parser
-# Constantes para facilitar la lectura y mantenimiento del codigo y los campos.
-VALCUOTPARTE=0
-VARDIARIA=1
-LAST30=2
-LAST90=3
-VARANUAL=4
-# EXCEPCIONES
+import jsonpickle
+
+# ================= EXCEPCIONES ================= #
 class NoNewData(Exception):
     pass
-# ================= CLASES del dominio ================= #
-class Historico:
-    'Registro histórico de los rendimientos de los fondos'
-    RendimientosDiarios=[]
-    def addRendimiento(self,renddiario):
-        RendimientoDiario.append(renddiario)
 
+# ================= CLASES del dominio ================= #
 class RendimientoDiario:
     'La instancia de RendimientoDiario contiene las tablas de fondos en pesos, dolares y letes.'
     def __init__(self, a_fecha, tablapes, tabladolar, tablaletras):
@@ -44,8 +35,22 @@ class Fondo:
     def __init__(self, nombre, los_indicadores):
         self.name = nombre
         self.indicadores = los_indicadores
-
+    def getName(self):
+        return self.name
+    def getIndicadores(self):
+        return self.indicadores
 # ================= FUNCIONES auxiliares ================= #
+
+def removeColTags(a_web):
+    col_tags = a_web.find_all("col")
+    [col_tag.unwrap() for col_tag in col_tags]
+    return a_web
+    # parametrizar estas dos funciones con orden sup/parametros (str y funcion la complican, podria hacer "col" una funcion dummy pero parece forzado)
+
+def removeEmptyTags(unaweb):
+    empty_tags = unaweb.find_all(lambda tag: not tag.contents) # si le paso un html string no elimina los <tr></tr> vaya uno a saber xq'
+    [empty_tag.extract() for empty_tag in empty_tags]
+    return unaweb
 
 def esCampoIndicador(td):
     return td.name == "td" and td["align"] == "right" and td.has_attr('class') #encapsulado pues es sensible a cambios
@@ -55,6 +60,7 @@ def getCamposIndicadores(amotherrow):
 
 def getName(arow):
     strCelda = arow.td.table.tr.td.string
+    # funciona solo para el primero, corregir para dollar y letes
     return strCelda.strip()
 
 def getIndicadores(amotherrow):
@@ -93,102 +99,98 @@ def dirPadre():
 def directorio(folderStr):
     return os.path.join(dirPadre(),folderStr) #dado un subdirectorio de la carpeta raiz del directorio, devuelve el path completo hacia él.
 
-def getHistoryDates(): # todo: resolver despues de implementar las instancias de clase. PRobablemente sea mas efectivo consultar la abstraccion que almacene esos datos mas inteligentes que un texto crudo (y de paso respeta el concepto de backup)
-    dir_datos = directorio("Data")
-    all_files = os.listdir(dir_datos)
-    pass
-    #print(all_files)
-
-def fechaRepetida(fecha):
-    historial_fechas = getHistoryDates()
-    pass
-    #return fecha in historial_fechas
-    ### Considerando.. crear un backup consultable, con cierto poder de respuesta ante tuFecha::file->fecha. Porque por dos lados estoy consultando la fecha, al archivo tokenizado y al historial.
-
 def esCategoria(row,strCat):
     return lastWord(row) == strCat
 
-def filtrarFondos(rows):
-    fpesos=rows[0:19]
-    fdol=rows[19:29]
-    fletes=rows[29:40]
-    return (fpesos,fdol,fletes) # comienzan por header categoria/headers/rowsfondos al mismo nivel de row.
-
 def getHeaders(row):
     headers=[]
-    [headers.append(th.string) for th in row.find_all("th")]
+    [headers.append(th.string.strip()) for th in row.find_all("th")]
     return headers
 
-def storeData(rowsfondo):
+def rowValida(row):
+    # hay html basura en el codigo, incluyendo un <tr><td></td></tr> que se disfraza de row
+    return row.td.has_attr("class")
+
+# ================= FIN FUNCIONES auxiliares ================= #
+
+def objectsToJSON(rend_diario,fecha_vig):
+    json_str = jsonpickle.pickler.encode(rend_diario)
+    json_dir = os.path.join(dirPadre(), "JSON")
+    file_name = "Fondos-%s.json" % fecha_vig
+    json_file = io.open(os.path.join(json_dir, file_name), "w+")
+    json_file.write(json_str)
+    print("FIN")
+
+def instanciarFondo(rowsfondo):
     Fondos = []
     moneda = lastWord(rowsfondo[0])
     headers = getHeaders(rowsfondo[1]) # armar dicc?
     onlyfondosrows = rowsfondo[2:] # me quedo solo con row de fondos
     for row in onlyfondosrows:
-        fondo = Fondo(getName(row), getIndicadores(row))
-        Fondos.append(fondo)
+        if rowValida(row):
+            fondo = Fondo(getName(row), getIndicadores(row))
+            Fondos.append(fondo)
     return FondosDeMoneda(moneda,Fondos)
 
-def guardarRendimiento(fecha,fpes,fdol,flet):
-    fondo_pesos = storeData(fpes)
-    fondo_dollars = storeData(fdol)
-    fondo_letras = storeData(flet)
-    RendimientoDiario(fecha,fondo_pesos,fondo_dollars,fondo_letras)
+def obtenerRendimientoDelDia(fecha_vigencia,fpes,fdol,flet):
+    fondo_pesos = instanciarFondo(fpes)
+    fondo_dollars = instanciarFondo(fdol)
+    fondo_letras = instanciarFondo(flet)
+    return RendimientoDiario(fecha_vigencia,fondo_pesos,fondo_dollars,fondo_letras)
+    # todo: Historico.addRendimiento( rd ) o eso va directo e json..
+
+def filtrarFondos(prerows):
+    rows = prerows[1:] # fecha ya extraida
+    fpesos=rows[0:19]
+    fdol=rows[19:27]
+    fletes=rows[27:40]
+    return (fpesos,fdol,fletes) # comienzan por header categoria/headers/rowsfondos al mismo nivel de row.
 
 def procesarFondos(rows, fecha):
-    true_rows = rows[1:]
-    fondosPesos, fondosDol, fondosLetes = filtrarFondos(true_rows)
-    guardarRendimiento(fecha,fondosPesos,fondosDol,fondosLetes)
-
-def removeColTags(a_web):
-    col_tags = a_web.find_all("col")
-    [col_tag.unwrap() for col_tag in col_tags]
-    return a_web
-# parametrizar estas dos funciones con orden sup/parametros (str y funcion la complican, podria hacer "col" una funcion dummy pero parece forzado)
-def removeEmptyTags(unaweb):
-    empty_tags = unaweb.find_all(lambda tag: not tag.contents)
-    [empty_tag.extract() for empty_tag in empty_tags]
-    return unaweb
+    fondosPesos, fondosDol, fondosLetes = filtrarFondos(rows)
+    rend_diario = obtenerRendimientoDelDia(fecha,fondosPesos,fondosDol,fondosLetes)
+    objectsToJSON(rend_diario,fecha)
 
 def limpiarWeb(absweb):
     return removeEmptyTags(removeColTags(absweb))
 
 def capturarFecha(web_sincols):
     textoFecha = web_sincols.div.table.tr.th.string
-    return parser.parse(textoFecha,fuzzy=True).strftime("%Y/%m/%d")
+    return parser.parse(textoFecha,fuzzy=True).strftime("%Y-%m-%d")
 
-def procesarDatos(unaWeb):
-    web = limpiarWeb(unaWeb)
+def procesarDatos(webcruda):
+    web_tokenizada = BeautifulSoup(webcruda, "html.parser")
+    web = limpiarWeb(web_tokenizada)
     fecha = capturarFecha(web)
     rows = web.div.table.find_all("tr", recursive=False)
-    procesarFondos(rows,fecha)
+    rend_diario = procesarFondos(rows,fecha)
 
 def backupSourceFile(webrio):
     try:
-        # *Considerar guardar el archivo html sin embellecer para menor dependencia y mayor confianza en la integridad de los datos.**
+        # backupea el archivo, independientemente que la fecha de la tabla a parsear sea identica a la del dia anterior. Proteccion ante errores de la pagina
         stringfecha = time.strftime("%Y%m%d")
         file_name = 'Fondos-%s.dat' % stringfecha
         data_dir = os.path.join(dirPadre(), "Data")
         html_file = io.open(os.path.join(data_dir,file_name), 'w', encoding="UTF-8")
-        # le escribo el arbol del html webrio prettified (tabulado)
-        html_tokenizado = BeautifulSoup(webrio, "html.parser")
-        # TODO: escribir sólo si la fecha no coincide con la del dia anterior. (function TableDate :: Tabla->String->Bool)
-        html_file.write(html_tokenizado.prettify())
-        return html_tokenizado
+        web_str = str(webrio, 'utf-8')
+        html_file.write(web_str.strip())
     except FileExistsError:
         print("ARCHIVO_YA_PROCESADO.")
         print("Los datos de rendimiento del día de hoy ya han sido procesados.")
         exit(-1)
 
 
+
 # ================= MAIN  ================= #
 url = "http://www.santanderrio.com.ar/ConectorPortalStore/Rendimiento"
 webrio = urllib.request.urlopen(url).read()
-bsweb = backupSourceFile(webrio)
-procesarDatos(bsweb)
-# 1. Imprimir tabla de valores del día
-# 2. Imprimir historico de fondo
-# 3. Sugerir un fondo según móvil de inversion y aversión al riesgo definidas por el usuario
+backupSourceFile(webrio)
+procesarDatos(webrio)
+
+
+# 1. Imprimir tabla de valores del día--> Desde JSON o valores en t. de ejecucion?
+# 2. Imprimir historico de fondo--> JSON FIND in many files by name.
+# 3. Sugerir un fondo según móvil de inversion(anti-inflacion, ahorro, ganancia) y aversión al riesgo (r.fija, variable, m.dinero) definidas por el usuario
 # 4. --help con correspondencia entre Tipo de Renta y perfil de inversor, con comentario.
 # 1.2 Exportar Tabla historico, puntual a excel
 # 3.2 Asociar tipo de renta a perfil de aversion.
